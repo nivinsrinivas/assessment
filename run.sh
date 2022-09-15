c0555648f2a3c49be45f67e060d8d9c0d6c00f71
nivinsrinivas <nivinsrinivas@gmail.com>
2022-09-15 19:04:57 +0530

    Print execution time

--- run.sh
+++ run.sh
@@ -1,3 +1,5 @@
+# Execution Start Time
+start=$(date +%s)
 # Build dependencies
 echo "=============================================================="
 echo "Building project dependencies"
@@ -7,7 +9,7 @@ chmod u+x build_dependencies.sh
 # Run black formatting
 echo "=============================================================="
 echo "Running black formatting on analysis.py"
-python -m black analysis.py
+pipenv run python3 -m black analysis.py
 
 # Run the spark job using spark-submit within the pipenv
 echo "=============================================================="
@@ -16,4 +18,9 @@ pipenv run spark-submit --master local[*] --py-files packages.zip analysis.py
 
 echo "=============================================================="
 echo "Successfully completed the Car Crash Analysis."
+echo "=============================================================="
 echo "Please check car_crash_analysis.log in root directory for the solutions and out Dataframes written in newly created output directory"
+echo "=============================================================="
+# Time elapsed
+echo "Elapsed Time: $(($end-$start)) seconds"
+echo "===========================DONE!============================"
