c2ec812d8c0fa83caf9176600ec0d240e604ca71
nivinsrinivas <nivinsrinivas@gmail.com>
2022-09-15 19:00:05 +0530

    Logging the flow of execution in run.sh

--- run.sh
+++ run.sh
@@ -1,15 +1,19 @@
 # Build dependencies
+echo "=============================================================="
 echo "Building project dependencies"
 chmod u+x build_dependencies.sh
 ./build_dependencies.sh
 
 # Run black formatting
+echo "=============================================================="
 echo "Running black formatting on analysis.py"
 python -m black analysis.py
 
 # Run the spark job using spark-submit within the pipenv
-echo "Running the spark-submit"
+echo "=============================================================="
+echo "Running the spark-submit..."
 pipenv run spark-submit --master local[*] --py-files packages.zip analysis.py
 
+echo "=============================================================="
 echo "Successfully completed the Car Crash Analysis."
-echo "Please check car_crash_analysis.log in root directory for the solutions and Dataframes written in newly created output directory"
+echo "Please check car_crash_analysis.log in root directory for the solutions and out Dataframes written in newly created output directory"
