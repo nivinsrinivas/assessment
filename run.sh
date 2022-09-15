8144f920f4c29494f250ee2c43c3efd3512da66c
nivinsrinivas <nivinsrinivas@gmail.com>
2022-09-15 19:14:41 +0530

    Removing black formatting step

--- run.sh
+++ run.sh
@@ -1,15 +1,9 @@
-# Execution Start Time
-start=$(date +%s)
 # Build dependencies
 echo "=============================================================="
 echo "Building project dependencies"
 chmod u+x build_dependencies.sh
 ./build_dependencies.sh
 
-# Run black formatting
-echo "=============================================================="
-echo "Running black formatting on analysis.py"
-black analysis.py
 
 # Run the spark job using spark-submit within the pipenv
 echo "=============================================================="
@@ -21,7 +15,4 @@ echo "Successfully completed the Car Crash Analysis."
 echo "=============================================================="
 echo "Please check car_crash_analysis.log in root directory for the solutions and out Dataframes written in newly created output directory."
 echo "=============================================================="
-# Time elapsed
-start=$(date +%s)
-echo "Elapsed Time: $(($end-$start)) seconds."
 echo "===========================DONE!============================"
